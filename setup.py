3fda3111bb561509841547cee43ff1937609213e
khusichoudhury <khusichoudhury001@gmail.com>
2024-05-17 12:54:05 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools  # type: ignore
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__="0.0.0"
+
+REPO_NAME = "TEXT-SUMMARIZER"
+AUTHOR_USER_NAME = "khusi"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "khusichoudhury001@gmail.com"
+
+setuptools .setup(
+     name=SRC_REPO,
+     version=__version__,
+     author=AUTHOR_USER_NAME, 
+     author_email=AUTHOR_EMAIL,
+     description="A small python package for NLP app", 
+     Long_description = long_description,
+     Long_description_content="text/markdown",
+     urL=f"https://github.com/(AUTHOR_USER_NAME)/(REPO_NAME)",
+     project_urls={
+       "Bug Tracker": f"https://github.com/(AUTHOR_USER_NAME)/(REPO NAME)/issues",     
+},
+     package_dir={"": "src"},
+     packages=setuptools. find_packages (where="src")
+)
